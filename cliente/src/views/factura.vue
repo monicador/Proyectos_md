76123423f5f008a0b7b0b948b0749530513d29d6
edithbutp <86961741+edithbutp@users.noreply.github.com>
2021-10-09 20:14:18 -0500

    Update factura.vue

--- cliente/src/views/factura.vue
+++ cliente/src/views/factura.vue
@@ -1,6 +1,37 @@
 <template>
     <div class="container">
-        <h1>Radicación</h1>
+        <h1>Facturas Radicadas</h1>
+
+        <b-alert
+            :show="dismissCountDown"
+            dismissible
+            :variant="mensaje.color"
+            @dismissed="dismissCountDown=0"
+            @dismiss-count-down="countDownChanged">
+        {{mensaje.texto}}
+</b-alert>
+
+
+
+        <form @submit.prevent="radicarfactura()">
+            <h3>Radicar Nuevas Facturas</h3>
+
+            <input type="text" class="form-control my-2" placeholder="No. Radicacion" v-model ="facturar.noradicacion">
+            <input type="text" class="form-control my-2" placeholder="Nit" v-model ="facturar.nit">
+            <input type="text" class="form-control my-2" placeholder="Prefijo" v-model ="facturar.prefijo">
+            <input type="text" class="form-control my-2" placeholder="No. Factura" v-model ="facturar.nofactura">
+            <input type="text" class="form-control my-2" placeholder="Nivel" v-model ="facturar.nivel">
+            <input type="text" class="form-control my-2" placeholder="Tipo Id" v-model ="facturar.tipoid">
+            <input type="text" class="form-control my-2" placeholder="Id Paciente" v-model ="facturar.idpaciente">
+            <input type="text" class="form-control my-2" placeholder="Nombre Paciente" v-model ="facturar.nombrepaciente">
+            <input type="text" class="form-control my-2" placeholder="Fecha Factura" v-model ="facturar.fechafactura">
+            <input type="text" class="form-control my-2" placeholder="Valor Factura" v-model ="facturar.valorfactura">
+            <input type="text" class="form-control my-2" placeholder="Estado" v-model ="facturar.estado">
+            <b-button class="btn-success my-2" type="submit">Radicar Factura</b-button>
+
+
+        </form>
+
         <table class="table">
 <thead>
     <tr>
@@ -48,6 +79,13 @@ export default {
 
             factura: [],
 
+            mensaje: {color: 'success', texto: ''},
+            dismissSecs: 5,
+            dismissCountDown: 0,
+
+
+            facturar:{noradicacion:"",nit:"",prefijo:"",nofactura:"",nivel:"",tipoid:"",idpaciente:"",nombrepaciente:"",fechafactura:"",valorfactura:"",estado:""}
+
         }
     },
 
@@ -70,6 +108,42 @@ export default {
                 console.log(e.response)
             })
 
+        },
+
+        radicarfactura(){
+
+            this.axios.post('/nueva-factura',this.facturar)
+            .then(res=>{
+                
+                this.factura.push(res.data);
+                this.facturar.noradicacion="";
+                this.facturar.nit="";
+                this.facturar.prefijo="";
+                this.facturar.nofactura="";
+                this.facturar.nivel="";
+                this.facturar.tipoid="";
+                this.facturar.idpaciente="";
+                this.facturar.nombrepaciente="";
+                this.facturar.fechafactura="";
+                this.facturar.valorfactura="";
+                this.facturar.estado="";
+                this.mensaje.color="success";
+                this.mensaje.texto="Factura agregada";
+                this.showAlert();
+
+
+
+            })
+            .catch(e=>{
+                console.log(e.response)
+        })
+        },
+
+        countDownChanged(dismissCountDown) {
+            this.dismissCountDown = dismissCountDown
+        },
+        showAlert() {
+            this.dismissCountDown = this.dismissSecs
         }
 
     }
